29444c61e33797d9c43401e05b4db3a5c039cd7f
Richard van de Kooij <ridicage@gmail.com>
2019-05-24 12:42:54 +0200

    icon strings added

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
     </title>
 
     <!-- Pagina Icoontjes -->
-    <link rel="shortcut icon" href="./img/logo/Watch4U.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="200x200" href="./img/logo/Watch4U.png">
-    <link rel="icon" href="./img/logo/Watch4U.svg" type="image/x-icon">
-    <link rel="icon" href="./img/logo/Watch4U.gif">
+    <link rel="shortcut icon" href="./img/icon/Watch4U.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="200x200" href="./img/icon/Watch4U.png">
+    <link rel="icon" href="./img/icon/Watch4U.svg" type="image/x-icon">
+    <link rel="icon" href="./img/icon/Watch4U.gif">
 
 </head>
 
