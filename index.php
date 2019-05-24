4244813aa33a5a032642a5d1181a597372801b78
Richard van de Kooij <ridicage@gmail.com>
2019-05-24 12:35:51 +0200

    icons: .gif .png .svg .ico

--- index.php
+++ index.php
@@ -18,6 +18,12 @@
         } ?>&trade;
     </title>
 
+    <!-- Pagina Icoontjes -->
+    <link rel="shortcut icon" href="./img/logo/Watch4U.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="200x200" href="./img/logo/Watch4U.png">
+    <link rel="icon" href="./img/logo/Watch4U.svg" type="image/x-icon">
+    <link rel="icon" href="./img/logo/Watch4U.gif">
+
 </head>
 
 <body>
--- index/css.php
+++ index/css.php
@@ -10,9 +10,3 @@
 <link rel="stylesheet" href="./css/style.css">
 <link rel="stylesheet" href="./css/navbar.css">
 <link rel="stylesheet" href="./css/content.css">
-
-<!-- Pagina Icoontjes -->
-<link rel="shortcut icon" href="./img/logo/Watch4U.ico">
-<link rel="apple-touch-icon-precomposed" sizes="200x200" href="./img/logo/Watch4U.png">
-<link rel="icon" href="./img/logo/Watch4U.svg" type="image/x-icon">
-<link rel="icon" href="./img/logo/Watch4U.gif">
