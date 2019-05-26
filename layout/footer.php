193a6a6cea9fa3d56344bf91dfadd865444b36bf
IJuanTM <iwan2510@gmail.com>
2019-05-26 21:33:50 +0200

    footer

---
+++ layout/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer container">
+        <div class="row">
+            <div class="col-4">
+                <h5 class="footer-header">About us</h5>
+                <ul class="footer-text">
+                    <li><a href="#">Company Information</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Reviews</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
