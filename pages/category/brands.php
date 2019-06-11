2557524424728763736082367126433a6ed67178
IJuanTM <iwan2510@gmail.com>
2019-06-11 09:56:31 +0200

    ok

--- pages/category/brands.php
+++ pages/category/brands.php
@@ -2,8 +2,8 @@
 <h1 class="content-text">Brands</h1>
 <hr class="content-row">
 
-<div class="category-group" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
-    <figure class="category-item">
+<div class="category-group">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
         <a href="./index.php?content=brands/apple">
             <img class="mx-auto my-auto" src="./img/logo/brands/apple.png">
             <figcaption>
@@ -12,7 +12,7 @@
         </a>
     </figure>
 
-    <figure class="category-item">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
         <a href="./index.php?content=brands/samsung">
             <img class="mx-auto my-auto" src="./img/logo/brands/samsung.png">
             <figcaption>
@@ -21,7 +21,7 @@
         </a>
     </figure>
 
-    <figure class="category-item">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
         <a href="./index.php?content=brands/lv">
             <img class="mx-auto my-auto" src="./img/logo/brands/louis_vuitton.png">
             <figcaption>
@@ -31,8 +31,8 @@
     </figure>
 </div>
 
-<div class="category-group" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
-    <figure class="category-item">
+<div class="category-group">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
         <a href="./index.php?content=brands/mk">
             <img class="mx-auto my-auto" src="./img/logo/brands/michael_kors.png">
             <figcaption>
@@ -41,7 +41,7 @@
         </a>
     </figure>
 
-    <figure class="category-item">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
         <a href="./index.php?content=brands/garmin">
             <img class="mx-auto my-auto" src="./img/logo/brands/garmin.png">
             <figcaption>
@@ -50,19 +50,19 @@
         </a>
     </figure>
 
-    <figure class="category-item">
-        <a href="">
-            <img class="mx-auto my-auto" src="./img/logo/brands/polar.png">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/fitbit">
+            <img class="mx-auto my-auto" src="./img/logo/brands/fitbit.png">
             <figcaption>
-                <h3 class="category-name">Polar</h3>
+                <h3 class="category-name">FitBit</h3>
             </figcaption>
         </a>
     </figure>
 </div>
 
-<div class="category-group" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
-    <figure class="category-item">
-        <a href="">
+<div class="category-group">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/fossil">
             <img class="mx-auto my-auto" src="./img/logo/brands/fossil.png">
             <figcaption>
                 <h3 class="category-name">Fossil</h3>
@@ -70,17 +70,17 @@
         </a>
     </figure>
 
-    <figure class="category-item">
-        <a href="">
-            <img class="mx-auto my-auto" src="./img/logo/brands/fitbit.png">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/polar">
+            <img class="mx-auto my-auto" src="./img/logo/brands/polar.png">
             <figcaption>
-                <h3 class="category-name">FitBit</h3>
+                <h3 class="category-name">Polar</h3>
             </figcaption>
         </a>
     </figure>
 
-    <figure class="category-item">
-        <a href="">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/casio">
             <img class="mx-auto my-auto" src="./img/logo/brands/casio.png">
             <figcaption>
                 <h3 class="category-name">Casio</h3>
@@ -89,9 +89,9 @@
     </figure>
 </div>
 
-<div class="category-group" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
-    <figure class="category-item">
-        <a href="">
+<div class="category-group">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/amazfit">
             <img class="mx-auto my-auto" src="./img/logo/brands/amazfit.png">
             <figcaption>
                 <h3 class="category-name">Amazfit</h3>
@@ -99,8 +99,8 @@
         </a>
     </figure>
 
-    <figure class="category-item">
-        <a href="">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/mobvoi">
             <img class="mx-auto my-auto" src="./img/logo/brands/mobvoi.png">
             <figcaption>
                 <h3 class="category-name">Mobvoi</h3>
@@ -108,8 +108,8 @@
         </a>
     </figure>
 
-    <figure class="category-item">
-        <a href="">
+    <figure class="category-item" data-aos="zoom-in" data-aos-anchor-placement="middle-bottom" data-aos-duration="700">
+        <a href="./index.php?content=brands/misfit">
             <img class="mx-auto my-auto" src="./img/logo/brands/misfit.png">
             <figcaption>
                 <h3 class="category-name">Misfit</h3>
