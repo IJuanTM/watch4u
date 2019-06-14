236275cec41be574694e5c1bf89bf9180af29807
IJuanTM <iwan2510@gmail.com>
2019-06-14 10:21:15 +0200

    help mij

---
+++ connect_db.php
@@ -0,0 +1,7 @@
+<?php
+define("HOSTNAME", "localhost");
+define("USERNAME", "u37477p32749_school");
+define("PASSWORD", "Watch4U");
+define("DBNAME", "u37477p32749_watch4u");
+
+$conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DBNAME);
---
+++ pages/brands/casio.php
@@ -0,0 +1,158 @@
+<?php
+include("./layout/modal.php");
+?>
+
+<hr class="content-row">
+<h1 class="content-text">Casio Collection</h1>
+<hr class="content-row">
+
+<!-- Casio special group -->
+<div class="item-group casio-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">Special Editions:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/special/black.png">
+        <figcaption>
+            <h3 class="item-name">Casio Pro-Trek Black</h3>
+            <h5 class="item-price">€999</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Casio lcw group -->
+<div class="item-group casio-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">LCW:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/lcw/black.png">
+        <figcaption>
+            <h3 class="item-name">Casio LCW Black</h3>
+            <h5 class="item-price">€599</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/lcw/blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio LCW Blue</h3>
+            <h5 class="item-price">€599</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/lcw/white.png">
+        <figcaption>
+            <h3 class="item-name">Casio LCW White</h3>
+            <h5 class="item-price">€599</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Casio g-shock gg group -->
+<div class="item-group casio-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">G-Shock GG:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gg/blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GG Blue</h3>
+            <h5 class="item-price">€749</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gg/beige_blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GG Beige Blue</h3>
+            <h5 class="item-price">€749</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gg/green_blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GG Green Blue</h3>
+            <h5 class="item-price">€749</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Casio g-shock gst group -->
+<div class="item-group casio-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">G-Shock GST:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gst/black.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GST Black</h3>
+            <h5 class="item-price">€499</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gst/blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GST Blue</h3>
+            <h5 class="item-price">€499</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gst/gray.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GST Gray</h3>
+            <h5 class="item-price">€449</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Casio g-shock gwg group -->
+<div class="item-group casio-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">G-Shock GWG:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gwg/black.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GWG Black</h3>
+            <h5 class="item-price">€799</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/casio/g_shock/gwg/blue.png">
+        <figcaption>
+            <h3 class="item-name">Casio G-Shock GWG Blue</h3>
+            <h5 class="item-price">€849</h5>
+        </figcaption>
+    </figure>
+
+</div>
