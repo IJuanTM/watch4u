5dd15d0d58b5e711c3ca17b05f2d56ab6df1d31a
Richard van de Kooij <ridicage@gmail.com>
2019-06-14 13:29:11 +0200

    Script map created

--- script/connect_db.php
+++ script/connect_db.php
@@ -20,12 +20,12 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" href="./css/error.css">
+		<link rel="stylesheet" href="../css/error.css">
 		<title>Error&trade;</title>
-		<link rel="shortcut icon" href="./img/icon/Watch.ico">
-		<link rel="apple-touch-icon-precomposed" sizes="200x200" href="./img/icon/Watch.png">
-		<link rel="icon" href="./img/icon/Watch.svg" type="image/x-icon">
-		<link rel="icon" href="./img/icon/Watch.gif">
+		<link rel="shortcut icon" href="../img/icon/Watch.ico">
+		<link rel="apple-touch-icon-precomposed" sizes="200x200" href="../img/icon/Watch.png">
+		<link rel="icon" href="../img/icon/Watch.svg" type="image/x-icon">
+		<link rel="icon" href="../img/icon/Watch.gif">
 	</head>
 	<body>
 		<div class='back-error'>
