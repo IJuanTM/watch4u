5700592cc6d2b54df5aaa4819a9b10a848d0f764
IJuanTM <iwan2510@gmail.com>
2019-06-14 11:38:07 +0200

    misfit 1

---
+++ pages/brands/misfit.php
@@ -0,0 +1,141 @@
+<?php
+include("./layout/modal.php");
+?>
+
+<hr class="content-row">
+<h1 class="content-text">Misfit Collection</h1>
+<hr class="content-row">
+
+<!-- Misfit command group -->
+<div class="item-group misfit-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">Command:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/command/black.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Command Black</h3>
+            <h5 class="item-price">€199</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/command/blue.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Command Blue</h3>
+            <h5 class="item-price">€199</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/command/silver.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Command Silver</h3>
+            <h5 class="item-price">€199</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Misfit path group -->
+<div class="item-group misfit-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">Path:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/path/black.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Path Black</h3>
+            <h5 class="item-price">€229</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/path/purple.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Path Purple</h3>
+            <h5 class="item-price">€229</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/path/pink.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Path Pink</h3>
+            <h5 class="item-price">€229</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Misfit phase group 1 -->
+<div class="item-group misfit-bg">
+
+    <div class="row">
+        <div class="col-1"></div>
+        <h1 class="group-text">Phase:</h1>
+        <div class="col-11"></div>
+    </div>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/black.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase Black</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/brown.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase Brown</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/purple.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase Purple</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/lime.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase Lime</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+</div>
+
+<!-- Misfit phase group 2 -->
+<div class="item-group misfit-bg">
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/white.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase White</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+    <figure class="shop-item">
+        <img class="mx-auto my-auto" src="./img/watches/brands/misfit/phase/blue.png">
+        <figcaption>
+            <h3 class="item-name">Misfit Phase Blue</h3>
+            <h5 class="item-price">€299</h5>
+        </figcaption>
+    </figure>
+
+</div>
