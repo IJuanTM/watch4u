59073310ea63f071de2534ffd783bd0a5f57ce5a
IJuanTM <iwan2510@gmail.com>
2019-06-20 11:25:55 +0200

    oekeke

--- pages/about.php
+++ pages/about.php
@@ -1,3 +1,15 @@
 <hr class="content-row">
-<h2>About Watch4U</h2>
+<h1>About Watch4U</h1>
 <hr class="content-row">
+
+<div class="about container">
+    <div class="row">
+        <div class="col-9">
+            <h4>About Us:</h4>
+            <h6 class="about-text">Watch4U is a company located in the heart of Utrecht. Our company is specialized in smartwatches and we offer a wide range of different brands, types and price ranges.</h6>
+            <h4>What we offer:</h4>
+            <h6 class="about-text">Our shop has a wide range of different smartwatches. We have hybrid watches, these are normal watches but with smart functions like; An accurate gps location, </h6>
+        </div>
+        <div class="col-3"><img src="./img/logo/Watch4U.svg" width="100%"></div>
+    </div>
+</div>
