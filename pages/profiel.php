d66dc6fadab22258b5302bc0c7d42957e09a86f6
RiDiCage <rvdkooij@live.nl>
2019-06-22 12:22:52 +0200

    edit profile page

--- pages/profiel.php
+++ pages/profiel.php
@@ -1,23 +1,11 @@
 <?php
-	/*include("../script/security.php");
+	// include("../script/security.php");
 
-    if ($userrole = 'Root'){
+    if ($userrole == 'Root'){
         echo 'Root Hello' . $firstname;
-    } else if ($userrole = 'Admin') {
+    } else if ($userrole == 'Admin') {
         echo 'Admin Hello' . $firstname;
     } else {
         echo 'Costumer Hello' . $firstnameS;
-    }*/
+    }
 ?>
-<html>
-    <head>
-        <title>
-            gebruiker
-        </title>
-    </head>
-
-    <body>
-        <h1>Hello User</h1>
-    
-    </body>
-</html>
