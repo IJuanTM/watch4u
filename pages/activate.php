a0f3760364ad8f678b30782e47402ad1374da2cd
RiDiCage <rvdkooij@live.nl>
2019-06-24 16:25:18 +0200

    activate page done

--- pages/activate.php
+++ pages/activate.php
@@ -6,20 +6,16 @@
 <form class="change_password-form container-fluid" name="change_password-form" action="./script/activate.php" method="POST">
 
     <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8">
+        <div class="col-2 col-md-3 col-lg-4"></div>
+        <div class="col-8 col-md-6 col-lg-4">
 
             <!-- Code row -->
             <div class="form-row">
 
-                <div class="form-group col-md-3">
-                </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-12">
                     <label class="form-text" for="inputcode">Code: <span class="required">*</span></label>
                     <input type="text" class="form-control" name="code" placeholder="Code example: 1234-5678-9012">
                     <span class="form-detail">Enter your code here.</span>
-                </div>
-                <div class="form-group col-md-3">
                 </div>
 
             </div>
@@ -27,14 +23,10 @@
             <!-- Email Check row -->
             <div class="form-row">
 
-                <div class="form-group col-md-3">
-                </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-12">
                     <label class="form-text" for="inputcode">Check Email: <span class="required">*</span></label>
                     <input type="email" class="form-control" name="email" placeholder="Email check to activate">
                     <span class="form-detail">Enter your email here.</span>
-                </div>
-                <div class="form-group col-md-3">
                 </div>
 
             </div>
@@ -42,18 +34,18 @@
             <!-- Button row -->
             <div class="form-row">
 
-                <div class="form-group col-md-3">
+                <div class="form-group col-3">
                 </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-6">
                     <button type="submit" class="btn form-btn btn-lg btn-block btn-dark">Activate email</button>
                 </div>
-                <div class="form-group col-md-3">
+                <div class="form-group col-3">
                 </div>
 
             </div>
 
         </div>
-        <div class="col-2"></div>
+        <div class="col-2 col-md-3 col-lg-4"></div>
     </div>
 
 </form>
