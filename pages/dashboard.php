652a76c060a067a275aebae463c22c7c80f8a761
IJuanTM <iwan2510@gmail.com>
2019-06-27 15:29:04 +0200

    au

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,94 +1,81 @@
-<?php
-    // include("./script/connect_db.php");
-	// include("./script/sanitize.php");
-	include("./script/security.php");
+<hr class="content-row">
+<h1>Dashboard</h1>
+<hr class="content-row">
 
-	/*if ($userrole == 'Root'){
+<div class="card-row">
 
-		// Root inhoud
-		echo '
-		<h1>Root</h1>
-		<br>
-		<p>Dit is de Super User :<br><br>
-			<table class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Voornaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($firstname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Tussenv.</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . strtolower($infix) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Achternaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($lastname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Email</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($email) . '</td>
-				</tr>
-			</table>
-		</p>
-		';
-	
-	} else if ($userrole == 'Admin'){
-		
-		// Admin inhoud
-		echo '
-		<h1>Administrator</h1>
-		<br>
-		<p>Dit is de Administrator :<br><br>
-			<table class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Voornaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($firstname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Tussenv.</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . strtolower($infix) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Achternaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($lastname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Email</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($email) . '</td>
-				</tr>
-			</table>
-		</p>
-		';
+	<div class="card bg-dark card-dashboard">
+		<h5 class="card-header">Your orders</h5>
+		<div class="card-body">
+			<div class="row">
 
-	} else if ($userrole == 'Customer'){
-		
-		// Klant inhoud
-		echo '
-		<h1>Customer</h1>
-		<br>
-		<p>Dit is de Klant :<br><br>
-			<table class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Voornaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($firstname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Tussenv.</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . strtolower($infix) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Achternaam</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($lastname) . '</td>
-				</tr>
-				<tr class="row">
-					<th class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">Email</td>
-					<td class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">' . ucwords($email) . '</td>
-				</tr>
-			</table>
-		</p>
-		';
-		
-	} else {
-		echo'geen gebruiker van deze site';
-    }*/
-?>
-<h1>Test User</h1>
+				<div class="col-3">
+					<i class="card-icon fas fa-clipboard-list"></i>
+				</div>
+
+				<div class="card-content col-9">
+
+					<div class="row">
+						<h6 class="card-text">View your current and past orders here.</h6>
+					</div>
+
+					<div class="card-btn-row row">
+						<button href="./index.php?content=" class="btn btn-block card-btn">Go to order</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="card bg-dark card-dashboard">
+		<h5 class="card-header">Your account information</h5>
+		<div class="card-body">
+			<div class="row">
+
+				<div class="col-3">
+					<i class="card-icon fas fa-user"></i>
+				</div>
+
+				<div class="card-content col-9">
+
+					<div class="row">
+						<h6 class="card-text">View your account page here.</h6>
+					</div>
+
+					<div class="card-btn-row row">
+						<button href="./index.php?content=" class="btn btn-block card-btn">Go to account</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="card bg-dark card-dashboard">
+		<h5 class="card-header">Change your password</h5>
+		<div class="card-body">
+			<div class="row">
+
+				<div class="col-3">
+					<i class="card-icon fas fa-unlock"></i>
+				</div>
+
+				<div class="card-content col-9">
+
+					<div class="row">
+						<h6 class="card-text">Change your password here.</h6>
+					</div>
+
+					<div class="card-btn-row row">
+						<button href="./index.php?content=" class="btn btn-block card-btn">Change password</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<hr class="content-row">
