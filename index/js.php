974a027df220e4dcd75fab9b0c25ec26adb58eea
RiDiCage <rvdkooij@live.nl>
2019-07-02 18:23:29 +0200

    link deleted

--- index/js.php
+++ index/js.php
@@ -4,7 +4,6 @@
 <script src="./js/bootstrap.min.js"></script>
 <script src="./js/aos.js"></script>
 <script src="https://kit.fontawesome.com/8564f1b892.js"></script>
-<script src="https://kit.fontawesome.com/576f7b044d.js"></script>
 
 <!-- Custom Java script files -->
 <script src="./js/script.js"></script>
