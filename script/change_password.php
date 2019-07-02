90102ad19b2ace3024399e9b67063f8ef62636aa
RiDiCage <rvdkooij@live.nl>
2019-07-02 15:35:44 +0200

    change password works

--- script/change_password.php
+++ script/change_password.php
@@ -9,26 +9,46 @@ $password0 = sanitize($_POST["password0"]);
 $password1 = sanitize(password_check($_POST["password1"]));
 $password2 = sanitize(password_check($_POST["password2"]));
 
+$sql = "SELECT * FROM `user` WHERE `password` = '$password0';";
+$result = mysqli_query($conn, $sql);
+
+$record = mysqli_fetch_assoc($result);
+$hash = $record["password"];
+
+if (password_verify($password0, $hash)) {
+	echo "Old Password is unknown<br>";
+}
+if (empty($iduser)) {
+    echo "You have to login first<br>";
+}
+if (empty($password0)) {
+    echo "You didn't enter the old password!<br>";
+}
 if (empty($password1)) {
-    echo "You didn't enter a password!<br>";
+    echo "You didn't enter a new password!<br>";
 }
 if (empty($password2)) {
-    echo "You didn't enter a password!<br>";
+    echo "You didn't enter a new password!<br>";
 }
 if ($password1 != $password2) {
     echo "The given passwords are not the same.<br>";
-} 
+}
+
+// echo 'Information:<br>' . $iduser . '<br>' . $password0 . '<br>' . $password1 . '<br>' . $password2 . '<br>' . $sql . '<br>' . $hash;
+
 if (strlen($password1) < 8) {
     echo "password to short, use 8 characters!<br>";
     include("./error2.php");
 } else {
     $blowfish_password = password_hash($password1, PASSWORD_BCRYPT);
 
-    $sql = "UPDATE `user` SET `password` = '$blowfish_password',
+    $sql1 = "UPDATE `user` SET `password` = '$blowfish_password'
             WHERE `user`.`iduser` = '$iduser';";
-echo $sql;
-    include("./error1.php");
-    // $result = mysqli_query($conn, $sql);
+    $result1 = mysqli_query($conn, $sql1);
 
-    // header("Refresh: 0; url=../index.php?content=account");
+    include("./error2.php");
+
+    header("Refresh: 0; url=../index.php?content=account");
 }
+
+?>
