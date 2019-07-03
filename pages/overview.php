539f5156c141c12742d344dfb51f3574a3936230
RiDiCage <rvdkooij@live.nl>
2019-07-03 22:25:20 +0200

    orders

--- pages/overview.php
+++ pages/overview.php
@@ -3,31 +3,31 @@
 
 		$sql = "SELECT * FROM `orderline`;";
 		$result = mysqli_query($conn, $sql);
-		$record = mysqli_fetch_assoc($result);
-		$id = $record["idproduct"];
-		$records = "";
-		
-		$sql1 = "SELECT * FROM `product` WHERE `idproduct` = $id";
-		$result1 = mysqli_query($conn, $sql1);
-		$record1 = mysqli_fetch_assoc($result1);
-		$records1 = "";
+		$records = '';
 
 		while ($record = mysqli_fetch_assoc($result)){
+
+			$id = $record["idproduct"];
+
+			$sql1 = "SELECT * FROM `product` WHERE `idproduct` = '$id'";
+			$result1 = mysqli_query($conn, $sql1);
+			$record1 = mysqli_fetch_assoc($result1);
+
 			$records .= "
 			<tr>
-				<td scope='row'>" . $record["idproduct"] . "</td>
+				<td scope='row'>" . $record["idorder"] . "</td>
 				<td scope='row'><img src='" . strtolower($record1["image"]) . "' width='60px' height='60px'</td>
 				<td scope='row'>" . ucwords($record1["name"]) . "</td>
 				<td scope='row'>" . $record["amount"] . "</td>
-				<td scope='row'>€ " . $record["price"] . "</td>
-				<td scope='row'>€ " . $record["total"] . "</td>
+				<td scope='row'> " . $record["price"] . "</td>
+				<td scope='row'> " . $record["total"] . "</td>
 				<td>
-					<a href='./script/update_product.php?idproduct=" . $record["idproduct"] . "'>
+					<a href='./script/update_product.php?idproduct=" . $id . "'>
 						<i class='fas fa-pen'></i>
 					</a>
 				</td>
 				<td>
-					<a href='./script/delete_product.php?idproduct=" . $record["idproduct"] . "'>
+					<a href='./script/delete_product.php?idproduct=" . $id . "'>
 						<i class='fas fa-times'></i>
 					</a>
 				</td>
@@ -35,34 +35,43 @@
 		}
 
 	} else if ($userrole == 'Admin'){
+		$sql = "SELECT * FROM `orderline`;";
+		$result = mysqli_query($conn, $sql);
+		$records = '';
+
 		while ($record = mysqli_fetch_assoc($result)){
+
+			$id = $record["idproduct"];
+
+			$sql1 = "SELECT * FROM `product` WHERE `idproduct` = '$id'";
+			$result1 = mysqli_query($conn, $sql1);
+			$record1 = mysqli_fetch_assoc($result1);
+
 			$records .= "
 			<tr>
-				<td scope='row'><img src='" . strtolower($record["image"]) . "' width='60px' height='60px'</td>
-				<td scope='row'>" . ucwords($record["name"]) . "</td>
-				<td scope='row'>" . $record["idproduct"] . "</td>
 				<td scope='row'>" . $record["idorder"] . "</td>
+				<td scope='row'><img src='" . strtolower($record1["image"]) . "' width='60px' height='60px'</td>
+				<td scope='row'>" . ucwords($record1["name"]) . "</td>
 				<td scope='row'>" . $record["amount"] . "</td>
-				<td scope='row'>€ " . $record["price"] . "</td>
-				<td scope='row'>€ " . $record["total"] . "</td>
+				<td scope='row'> " . $record["price"] . "</td>
+				<td scope='row'> " . $record["total"] . "</td>
 				<td>
-					<a href='./script/update_product.php?idproduct=" . $record["idproduct"] . "'>
+					<a href='./script/update_product.php?idproduct=" . $id . "'>
 						<i class='fas fa-pen'></i>
 					</a>
 				</td>
 				<td>
-					<a href='./script/delete_product.php?idproduct=" . $record["idproduct"] . "'>
+					<a href='./script/delete_product.php?idproduct=" . $id . "'>
 						<i class='fas fa-times'></i>
 					</a>
 				</td>
 			</tr>";
 		}
+
 	} else if ($userrole == 'Customer') {
 		header("Refresh: 0; url=../index.php?content=dashboard");
-
 	} else {
 		header("Refresh: 0; url=../index.php?content=homepage");
-
 	}
 ?>
 <?php
@@ -113,28 +122,28 @@
         <hr class="content-row">
 		<br>
 		<div class="container">
-        <table class="table table-dark table-responsive" style="border:0;">
+			<table class="table table-dark table-responsive" style="border:0;">
 				<thead style="color:gold;">
-						<tr>
+					<tr>
+						<th scope="col">ID Order</th>
 						<th scope="col">Image</th>
 						<th scope="col">Name</th>
-						<th scope="col">ID Product</th>
-						<th scope="col">ID Order</th>
-						<th scope="col">Amount</th>
-						<th scope="col">Price</th>
-						<th scope="col">Total</th>
-						<th scope="col"></th>
-						<th scope="col"></th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Total</th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
 					</tr>
 				</thead>
-                <tbody>
-                    ' . $records0 . '
+				<tbody>
+					' . $records . '
                     <tr>
                         <td scope="row">
                             <a href="./script/create_product.php" style="font-size:40px;">
                                 <i class="fas fa-file-medical"></i>
                             </a>
                         </td>
+                        <td scope="row"></td>
                         <td scope="row"></td>
                         <td scope="row"></td>
                         <td scope="row"></td>
